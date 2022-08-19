f63aacd2f44d3ab64720b4f2b452837cd504b8a2
MaximilianSchreff <111394953+MaximilianSchreff@users.noreply.github.com>
2022-08-19 22:09:28 +0200

    Add files via upload

---
+++ src/eventHandler.js
@@ -0,0 +1,39 @@
+document.addEventListener("keydown", function() {
+    if (event.code === "KeyA") {
+        let audio = new Audio("../Audios/A.mp3");
+        audio.play();
+    } else if (event.code === "KeyS") {
+        let audio = new Audio("../Audios/S.mp3");
+        audio.play();
+    } else if (event.code === "KeyD") {
+        let audio = new Audio("../Audios/D.mp3");
+        audio.play();
+    } else if (event.code === "KeyF") {
+        let audio = new Audio("../Audios/F.mp3");
+        audio.play();
+    } else if (event.code === "KeyG") {
+        let audio = new Audio("../Audios/G.mp3");
+        audio.play();
+    } else if (event.code === "KeyH") {
+        let audio = new Audio("../Audios/H.mp3");
+        audio.play();
+    } else if (event.code === "KeyJ") {
+        let audio = new Audio("../Audios/J.mp3");
+        audio.play();
+    } else if (event.code === "KeyW") {
+        let audio = new Audio("../Audios/W.mp3");
+        audio.play();
+    }  else if (event.code === "KeyE") {
+        let audio = new Audio("../Audios/E.mp3");
+        audio.play();
+    } else if (event.code === "KeyT") {
+        let audio = new Audio("../Audios/T.mp3");
+        audio.play();
+    } else if (event.code === "KeyY") {
+        let audio = new Audio("../Audios/Y.mp3");
+        audio.play();
+    } else if (event.code === "KeyU") {
+        let audio = new Audio("../Audios/U.mp3");
+        audio.play();
+    }
+});
